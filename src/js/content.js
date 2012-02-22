0c4a035efe21b02bf161a843084f7f7790fc5dc7
answeror <answeror@gmail.com>
2012-02-22 11:36:29 +0800

    should i include .pem here?

--- src/js/content.js
+++
@@ -1,9 +0,0 @@
-// learn form { http://stackoverflow.com/questions/3937000/chrome-extension-accessing-localstorage-in-content-script }
-chrome.extension.sendRequest({ method: "getShortcutKey" }, function (response) {
-	shortcut.add(response.add, function () {
-		chrome.extension.sendRequest({ command: "add" }, function (response) { });
-	});
-	shortcut.add(response.manage, function () {
-		chrome.extension.sendRequest({ command: "manage" }, function (response) { });
-	});
-});
